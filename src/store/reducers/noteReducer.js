156ab0300b2559bae3092527d6dd4716a9ba4bc8
Alexey <guitar19823@gamil.com>
2021-01-19 12:46:38 +0300

    js_to_ts

--- src/store/reducers/noteReducer.js
+++ src/store/reducers/noteReducer.js
@@ -36,8 +36,4 @@ const handlers = {
   DEFAULT: state => state,
 };
 
-export const noteReducer = (state = initialState, action) => {
-  const handler = handlers[action.type] || handlers.DEFAULT;
-
-  return handler(state, action);
-};
+export const noteReducer = (state = initialState, action) => (handlers[action.type] || handlers.DEFAULT)(state, action);
